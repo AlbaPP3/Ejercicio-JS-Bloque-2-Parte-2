43e42906538243d64d0012bb49a9eb2989b1e8a6
AlbaPP3 <95907455+AlbaPP3@users.noreply.github.com>
2023-02-14 12:32:27 +0100

    2 files modified

--- Ejercicio4.js
+++ Ejercicio4.js
@@ -8,3 +8,26 @@ let personas = [
   { nombre: 'Alberto', apellido: 'Rus', edad: 24 },
   { nombre: 'Alberto', apellido: 'Rodriguez', edad: 80 },
 ];
+let personasOrdenadas = personas.sort((a, b) => {
+  if (a.nombre < b.nombre) {
+    return -1;
+  } else if (a.nombre > b.nombre) {
+    return 1;
+  } else {
+    if (a.edad < b.edad) {
+      return -1;
+    } else if (a.edad > b.edad) {
+      return 1;
+    } else {
+      if (a.apellido < b.apellido) {
+        return -1;
+      } else if (a.apellido > b.apellido) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+});
+
+console.log(personasOrdenadas);
